6f9653c476af546b0402d3571806575ed3507418
Sergio Aliceral <sergio.aliceral@sesicni.com.br>
2021-03-02 16:20:49 -0300

    commit inicial

---
+++ src/main/java/br/com/aliceraltecnologia/HelloApplication.java
@@ -0,0 +1,21 @@
+package br.com.aliceraltecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping("hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
